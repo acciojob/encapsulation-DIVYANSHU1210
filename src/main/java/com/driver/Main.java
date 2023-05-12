a581c94dc80f3eb421bce3f7b44cfec72ddc7d6b
Divyanshu Joshi <divyanshujoshi1012@gmail.com>
2023-05-12 23:44:22 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        try {
+            obj.name = "tom";
+        }
+        catch (Exception e){
+            System.out.print("you might seen some error, please not it down in commnets");
+        }
+
+        obj.setname("tom");
+        System.out.print(obj.getname());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public String getname(){
+        return this.name;
+    }
+
+    public void setname(String n){
+        this.name = n;
+    }
 }
