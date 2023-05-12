7d9f75624b85adfdc3e34ec1c496bdd522dbae81
Divyanshu Joshi <divyanshujoshi1012@gmail.com>
2023-05-12 23:57:19 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
 
-        try {
-            obj.name = "tom";
-        }
-        catch (Exception e){
-            System.out.print("you might seen some error, please not it down in commnets");
-        }
+//        try {
+//            obj.name = "tom";
+//        }
+//        catch (Exception e){
+//            System.out.print("you might seen some error, please not it down in commnets");
+//        }
 
         obj.setname("tom");
         System.out.print(obj.getname());
